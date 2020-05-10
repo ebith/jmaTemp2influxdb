e1153a7fb0e540165817129dff320651165c607e
ebith <ebith.h@gmail.com>
2020-05-10 23:14:57 +0900

    add humidity and pressure

--- index.js
+++ index.js
@@ -7,34 +7,37 @@ const url = 'https://www.jma.go.jp/jp/amedas_h/today-64036.html?areaCode=000&gro
   const browser = await chromium.launch();
   const page = await browser.newPage();
   await page.goto(url);
-  const temperature = await page.evaluate(() => {
+  const result = await page.evaluate(() => {
     const today = new Date();
     for (const tr of document.querySelector('table#tbl_list').querySelectorAll('tr')) {
       const hour = tr.querySelector('td:nth-child(1)').textContent - 0;
       if (hour === today.getHours() || (hour === 24 && today.getHours() === 0)) {
         const temperature = tr.querySelector('td:nth-child(2)').textContent - 0;
-        if (!Number.isNaN(temperature)) {
-          return temperature;
-        }
+        const humidity = tr.querySelector('td:nth-child(7)').textContent - 0;
+        const pressure = tr.querySelector('td:nth-child(8)').textContent - 0;
+	return [{temperature}, {humidity}, {pressure}];
       }
     }
   });
   await browser.close();
-  if (temperature) {
-    const today = new Date();
-    today.setHours(today.getHours(), 0, 0);
-    const points = [
-      {
-        measurement: 'jma',
-        tags: {
-          location: 'nara',
-        },
-        fields: {
-          temperature,
-        },
-        timestamp: today,
+  const fields = result.filter((value) => {
+    return !Number.isNaN(Object.values(value)[0]);
+  }).reduce((acc, cur) => {
+     const key = Object.keys(cur);
+     acc[key] = cur[key];
+     return acc;
+  });
+  const today = new Date();
+  today.setHours(today.getHours(), 0, 0);
+  const points = [
+    {
+      measurement: 'jma',
+      tags: {
+        location: 'nara',
       },
-    ];
-    influx.writePoints(points).catch((error) => console.error(error));
-  }
+      fields,
+      timestamp: today,
+    },
+  ];
+  influx.writePoints(points).catch((error) => console.error(error));
 })();
